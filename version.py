a5ead488c5ee3c705a89e6b9d5927dbcfeb00420
sparsh-mehta <i.sparshmehta@gmail.com>
2022-10-12 11:57:02 -0600

    Reset

--- version.py
+++ version.py
@@ -1,11 +1,3 @@
 import numpy
 
 print("Numpy Version: ", numpy.__version__)
-print("Deep")
-print("deep1")
-print("deep2")
-print("deep3")
-print("sparsh3")
-print("testbranch")
-print("testbranch1")
-print("sparsh4")
